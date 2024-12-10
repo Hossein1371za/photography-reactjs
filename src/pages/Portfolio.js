7af2e13ea8d3b5552e4a1181289d62360f1e06d6
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-12-10 22:52:27 +0330

    portfolio section

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+
+import Image1 from "../img/portfolio/1.png";
+import Image2 from "../img/portfolio/2.png";
+import Image3 from "../img/portfolio/3.png";
+import Image4 from "../img/portfolio/4.png";
+
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className="section">
+      <div className="container mx-auto h-full relative">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-right pt-24 lg-pt-36 pb-8">
+          <div className="z-10 flex flex-col items-center justify-center lg:items-start">
+            <h1 className="h1 font-bold">نمونه کارها</h1>
+            <p className="mb-12 max-w-sm text-[#7a7979]">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان{" "}
+              <b className="text-[#3d3c3c]">
+                گرافیک است چاپگرها و متون بلکه روزنامه
+              </b>{" "}
+              و مجله در ستون و سطرآنچنان که
+              <br />
+              <br />
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+            </p>
+            <Link to={"/contact"} className="btn mb-[30px]">ارتباط با من</Link>
+          </div>
+          <div className="grid grid-cols-2 lg:gap-2">
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image1} alt="" />
+            </div>
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image2} alt="" />
+            </div>
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image3} alt="" />
+            </div>
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image4} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
