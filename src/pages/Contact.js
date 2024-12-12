61adc65dcb0cd4e76bb2594a6a0440dea73c7ec7
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-12-12 18:13:31 +0330

    animation pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 import WomanImg from "../img/contact/woman.png";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const Contact = () => {
   return (
-    <section className="section">
+    <section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start pt-36 lg:pt-24 gap-x-8 text-center lg:text-right">
-          <div className="hidden lg:flex bg-[#eaf7f9] absolute bottom-0 left-0 right-0 top-72 -z-10"></div>
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+            className="hidden lg:flex bg-[#eaf7f9] absolute bottom-0 left-0 right-0 top-72 -z-10"
+          ></motion.div>
           <div className="lg:flex-1 lg:pt-32 px-4">
             <h1 className="h1 font-bold">راه های ارتباطی</h1>
             <p className="mb-12">پیشنهادات خود را با من به اشتراک بگذارید.</p>
@@ -25,16 +39,24 @@ const Contact = () => {
                 />
               </div>
               <input
-                  type="text"
-                  placeholder="پیام شما..."
-                  className="outline-none border-b border-b-primary h-[60px] bg-transparent w-full pl-3 placeholder:text-[#757879]"
-                />
-                <button className="btn mb[30px] mx-auto lg:mx-0 self-start">ارسال نظر</button>
+                type="text"
+                placeholder="پیام شما..."
+                className="outline-none border-b border-b-primary h-[60px] bg-transparent w-full pl-3 placeholder:text-[#757879]"
+              />
+              <button className="btn mb[30px] mx-auto lg:mx-0 self-start">
+                ارسال نظر
+              </button>
             </form>
           </div>
-          <div className="lg:flex-1">
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{transition:transition1,duration:1.5}}
+            className="lg:flex-1"
+          >
             <img src={WomanImg} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,13 +6,27 @@ import Image3 from "../img/portfolio/3.png";
 import Image4 from "../img/portfolio/4.png";
 
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const Portfolio = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-right pt-24 lg-pt-36 pb-8">
-          <div className="z-10 flex flex-col items-center justify-center lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={transition1}
+            className="z-10 flex flex-col items-center justify-center lg:items-start"
+          >
             <h1 className="h1 font-bold">نمونه کارها</h1>
             <p className="mb-12 max-w-sm text-[#7a7979]">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
@@ -26,25 +40,43 @@ const Portfolio = () => {
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
               استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
             </p>
-            <Link to={"/contact"} className="btn mb-[30px]">ارتباط با من</Link>
-          </div>
+            <Link to={"/contact"} className="btn mb-[30px]">
+              ارتباط با من
+            </Link>
+          </motion.div>
           <div className="grid grid-cols-2 lg:gap-2">
             <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image1} alt="" />
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image1}
+                alt=""
+              />
             </div>
             <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image2} alt="" />
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image2}
+                alt=""
+              />
             </div>
             <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image3} alt="" />
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image3}
+                alt=""
+              />
             </div>
             <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500" src={Image4} alt="" />
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image4}
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
